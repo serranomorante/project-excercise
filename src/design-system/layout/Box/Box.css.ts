f2442c948e0d562f7af32754f3e54312f1519a2b
Patricio Serrano <serranomorantepatricio@gmail.com>
2025-03-05 12:29:41 +0100

    chore(design-system): improve handling box spaces

--- src/design-system/layout/Box/Box.css.ts
+++ src/design-system/layout/Box/Box.css.ts
@@ -2,9 +2,6 @@ import { style } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
 
 export const boxVariants = recipe({
-  base: {
-    padding: 40
-  },
   variants: {
     variant: {
       default: {},
@@ -28,21 +25,28 @@ export const boxVariants = recipe({
 export const boxChild = style({
   selectors: {
     [`${boxVariants.classNames.variants.container.true} &`]: {
+      flex: '1 0 auto',
+      alignSelf: 'stretch',
+      padding: 30,
       '@media': {
         'screen and (min-width: 640px)': {
           maxWidth: '640px',
         },
         'screen and (min-width: 768px)': {
           maxWidth: '768px',
+          padding: 40,
         },
         'screen and (min-width: 1024px)': {
-          maxWidth: '1024px',
+          maxWidth: '768px',
+          padding: 40,
         },
         'screen and (min-width: 1280px)': {
-          maxWidth: '1280px',
+          maxWidth: '768px',
+          padding: 40,
         },
         'screen and (min-width: 1536px)': {
-          maxWidth: '1536px',
+          maxWidth: '768px',
+          padding: 40,
         },
       }
     }
